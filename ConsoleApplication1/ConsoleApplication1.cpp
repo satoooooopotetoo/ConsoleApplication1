8820ff6556f65cf05c98a6ac3a110e42e9135334
さとう <k021g1454@g.neec.ac.jp>
2022-02-03 15:07:03 +0900

    satou

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,7 +5,10 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "satoupoteto\n";
+    std::cout << "satoupoteto\n";
+
+
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
